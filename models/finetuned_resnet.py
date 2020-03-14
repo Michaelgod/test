14b46865d480b43d1715c22b431656111acff39f
Michaelgod <381239675@qq.com>
2020-03-14 14:15:14 +0800

    test

---
+++ models/finetuned_resnet.py
@@ -0,0 +1,45 @@
+import os
+
+from keras.layers import Dense, Flatten, Dropout
+from keras.models import Model
+
+from models.resnet50 import ResNet50
+
+N_CLASSES = 101
+IMSIZE = (216, 216, 3)
+
+
+def finetuned_resnet(include_top, weights_dir):
+    """
+
+    :param include_top: True for training, False for generating intermediate results for
+                        LSTM cell
+    :param weights_dir: path to load finetune_resnet.h5
+    :return:
+    """
+    base_model = ResNet50(include_top=False, weights='imagenet', input_shape=IMSIZE)
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    x = base_model.output
+    x = Flatten()(x)
+    x = Dense(2048, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.5)(x)
+
+    if include_top:
+        x = Dense(N_CLASSES, activation='softmax')(x)
+
+    model = Model(inputs=base_model.input, outputs=x)
+    if os.path.exists(weights_dir):
+        model.load_weights(weights_dir, by_name=True)
+
+    return model
+
+
+if __name__ == '__main__':
+    model_save_path = 'E:/ActionRecognition/models/'
+    model = finetuned_resnet(include_top=True, weights_dir='')
+    model.save('finetuned_resnet.h5', model_save_path, include_optimizer=True)
+    print(model.summary())
